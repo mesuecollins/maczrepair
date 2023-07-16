22d395012d4a35acd7ee084654925abcffeed1b0
macz <mesue.collins2012@gmail.com>
2023-07-16 03:08:59 +0300

    carousel speed adjusted and previous modifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,10 +160,10 @@ document.addEventListener('DOMContentLoaded', () => {
    * Init swiper slider with 2 slides at once in desktop view
    */
   new Swiper('.slides-2', {
-    speed: 600,
+    speed: 1000,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 10000,
       disableOnInteraction: false
     },
     slidesPerView: 'auto',
